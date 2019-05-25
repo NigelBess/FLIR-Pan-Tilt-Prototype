43fcc3e92bfd8f50ffbd2eecc4edba0b78ca1187
NigelBess <nigelbess@gmail.com>
2019-05-25 16:52:45 -0700

    First commit

---
+++ Matlab/getTMCLValues.m
@@ -0,0 +1,21 @@
+clear all;
+clc;
+
+vRpm = 1340;
+n =200;
+vInt = 2047;
+
+fSpace = fliplr(2.^(0:8));%possible microstep range
+for f = fSpace
+    vPps = vRpm*n*f/60;
+    pulseDiv = log2(16E6*vInt/(2048*32*vPps));
+    if(isreal(pulseDiv) && pulseDiv>0)
+        break;
+    end
+end
+fprintf("Optimal Microstep: "+f+"\n");
+pulseDiv = floor(pulseDiv);
+fprintf("pulseDiv: "+pulseDiv+"\n");
+vPps = 16E6*vInt/(2^pulseDiv*2048*32);
+vRpm = 60*vPps/n/f;
+fprintf("max speed: "+vRpm+" RPM\n");
